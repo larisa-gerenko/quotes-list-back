07db193e1c518d86308fd9798d09395f5b34cd65
Larisa <larisavalgutova@gmail.com>
2021-09-10 11:31:14 +0300

    first commit

---
+++ app/Http/Controllers/QuotesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quote;
+use Illuminate\Http\Request;
+
+class QuotesController extends Controller
+{
+
+    public function list()
+    {
+        $allQuotes = Quote::all();
+
+        return response()->json($allQgit inituotes);
+    }
+
+    public function  add(Request $request) {
+        $newQuote = new Quote();
+        $newQuote->name = $request->input('name');
+        $newQuote->text = $request->input('text');
+        $newQuote->save();
+    }
+}
+
